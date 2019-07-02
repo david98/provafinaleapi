1ffff8d9e83acb0ba19b8ad2ae7925809eca9254
Davide Volta <dvolta@Davides-MacBook-Pro.local>
2019-07-02 21:00:18 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/provafinaleapi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/provafinaleapi.dir/main.c.o"
+  "provafinaleapi.pdb"
+  "provafinaleapi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/provafinaleapi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct generic_entity {
+    char *name;
+    int id;
+};
+
+void add_entity(struct generic_entity *entity_list){
+
+}
+
+int main() {
+
+    struct generic_entity *entity_list, *relations_list;
+    int next_entity_id = 0, next_relation_id = 0;
+
+    printf("Hello, World!\n");
+    return 0;
+}
